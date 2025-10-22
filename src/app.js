35e02449c1d2fca434bc837563a8cc53a4f89e6d
Ydefault <default>
2025-10-22 16:39:17 -0600

    changing overall desgin

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-//app.use(helmet());
-//app.use(cors());
+app.use(helmet());
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form posts (if needed)
